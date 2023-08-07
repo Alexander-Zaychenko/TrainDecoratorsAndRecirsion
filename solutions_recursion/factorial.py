875d9877d0cf148f54b4ac49cf8a750e7ee39d34
Alexander-Zaychenko <alexzay2012@yandex.ru>
2023-08-07 11:45:23 +0300

    add one solution

--- solutions_recursion/factorial.py
+++ solutions_recursion/factorial.py
@@ -2,3 +2,14 @@ def factorial(number):
     if number == 1:
         return number
     return number * factorial(number - 1)
+
+
+def test_factorial():
+    print('Testing... ')
+    # start test
+    assert factorial(0) == 1
+    # end test
+    print('Tests done successfully! Nice!')
+
+
+test_factorial()
